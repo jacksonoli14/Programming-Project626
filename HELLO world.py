3c6fca12b26d0286fe39f8ae06fb9dd6dc1b17d9
temifasoranti <66045884+temifasoranti@users.noreply.github.com>
2020-11-19 16:21:33 -0500

    Add files via upload

---
+++ HELLO world.py
@@ -0,0 +1,8 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("hello world")
+hello world
+>>> print ("hi")
+hi
+>>> 
